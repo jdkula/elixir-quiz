9285c66c8264daabab88c371308a86070bc24f62
Jonathan Kula <jonathan@jdkula.dev>
2020-07-17 03:09:32 -0700

    Add animation for the GlobalTimer via HideToggle

--- components/HideToggle.tsx
+++ components/HideToggle.tsx
@@ -1,12 +1,14 @@
 import { ReactNode, ReactElement, useState } from "react";
-import { Switch, Box, FormControlLabel } from "@material-ui/core";
+import { Switch, Box, FormControlLabel, Slide } from "@material-ui/core";
 
-export default function HideToggle({ children, label }: { children: ReactNode; label?: string }): ReactElement {
+export default function HideToggle({ children, label }: { children: ReactElement; label?: string }): ReactElement {
     const [shown, setShown] = useState(false);
 
     return (
         <Box component="span" display="flex" alignItems="center">
-            {shown && children}
+            <Slide direction="down" in={shown} mountOnEnter unmountOnExit>
+                {children}
+            </Slide>
             <Box mx={2}>
                 <FormControlLabel
                     control={<Switch checked={shown} onChange={(e) => setShown(e.target.checked)} color="default" />}
