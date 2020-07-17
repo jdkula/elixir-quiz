a3591ebe1b11aa239658882379dac7a3eecd4bb7
Jonathan Kula <jonathan@jdkula.dev>
2020-07-17 01:21:12 -0700

    Remove the elixirs endpoint, it's a constant and we can just use it.

--- pages/api/elixirs.ts
+++
@@ -1,9 +0,0 @@
-import { getElixirTypes } from "~/lib/elixir";
-import { NextApiHandler } from "next";
-
-const Elixirs: NextApiHandler = async (_, res) => {
-    res.setHeader("Cache-Control", "max-age=3600, public");
-    res.json(getElixirTypes());
-};
-
-export default Elixirs;
