93de18c4aa873a0644d5691ca2713315b84b907e
Jonathan Kula <jonathan@jdkula.dev>
2020-07-21 03:02:59 -0700

    Ensure MUI styles are also being rendered correctly on the server.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,15 +7,17 @@
 
 import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@material-ui/core';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
         const sheet = new ServerStyleSheet();
+        const muiSheets = new ServerStyleSheets();
         const originalRenderPage = ctx.renderPage;
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
-                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                    enhanceApp: (App) => (props) => muiSheets.collect(sheet.collectStyles(<App {...props} />)),
                 });
 
             const initialProps = await Document.getInitialProps(ctx);
@@ -25,6 +27,7 @@ class MyDocument extends Document {
                     <>
                         {initialProps.styles}
                         {sheet.getStyleElement()}
+                        {muiSheets.getStyleElement()}
                     </>
                 ),
             };
